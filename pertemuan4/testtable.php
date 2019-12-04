3c3f3888bfa60ad1fe0b75b0e08a356d405a2695
Apif Supriadi <afs.supriadi@gmail.com>
2019-12-04 10:40:05 +0700

    inisiasi pertama

---
+++ pertemuan4/testtable.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>
+                <label for="nama">Nama Lengkap</label>
+                <td>:</td>
+                <td><input type="text" name="nama"></td>
+            </td>
+        </tr>
+        
+        <tr>
+            <td>
+                <label for="nama">Jender</label>
+                <td>:</td>
+                <td>
+                    <input type="radio" value="Laki-Laki" name="jender">
+                    <input type="radio" value="Laki-Laki" name="jender">
+                </td>
+            </td>
+        </tr>
+
+        <tr>
+            <td><label for="">Program Studi</label></td>
+        </tr>
+    </table>
+</body>
+</html>
